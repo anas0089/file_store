f76d9db6cba996f632ff326b358d5b2ff7ab9c52
anas0089 <85271497+anas0089@users.noreply.github.com>
2021-08-08 09:45:55 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ class Config(object):
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("-1001482138748", None)
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001482138748".split())
 	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
